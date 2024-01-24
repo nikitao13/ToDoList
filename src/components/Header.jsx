4dd4cfa5553ea9a7b7ef9d370349bf45c0490d54
Nikita O'Keeffe <snow@Nikitas-MacBook-Air.local>
2024-01-24 19:45:30 +1100

    test commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header className="h-10 pl-3 bg-blue-300 flex flex-col justify-center">
-            <h1 className="w-max text-lg">To-do List</h1>
+            <h1 className="w-max text-lg">ToDo List</h1>
         </header>
     )
 }
